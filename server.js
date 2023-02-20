4b603b139ca6136221f95db739505c74b040b305
Kavitha Suresh Kumar <kavi2002suresh@gmail.com>
2023-02-20 10:20:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-const PORT = process.env.PORT |80;
+const PORT = process.env.PORT |8080;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
